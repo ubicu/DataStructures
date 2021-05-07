7bf228bb6a8671ebb5558f2f50c963b74614b406
Ham Rara <hmrara01@gmail.com>
2021-05-07 14:55:16 -0400

    Adding new tests for StackTest

--- DataStructures.Test/Stack/StackTest.cs
+++ DataStructures.Test/Stack/StackTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace DataStructures.Test.Stack
 {
@@ -11,11 +12,88 @@ namespace DataStructures.Test.Stack
 
         public StackTest()
         {
-
+            stacks.Add(new ListStack<int>());
+            stacks.Add(new ArrayStack<int>());
+            stacks.Add(new IntStack(2));
         }
+
+        [Fact]
+        public void TestEmptyStackOperation()
+        {
+            foreach (var stack in stacks)
+            {
+                Assert.True(stack.isEmpty());
+                Assert.Equal(0, stack.size());
+            }
+        }
+
+        [Fact]
+        public void TestPopOperation_EmptyStacks()
+        {
+            foreach (var stack in stacks)
+            {
+                var ex = Assert.Throws<InvalidOperationException>(() => stack.pop());
+                Assert.Equal($"Stack is empty", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void TestPeekOperation_EmptyStacks()
+        {
+            foreach (var stack in stacks)
+            {
+                var ex = Assert.Throws<InvalidOperationException>(() => stack.peek());
+                Assert.Equal($"Stack is empty", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void TestPushAndPeekOperation()
+        {
+            foreach (var stack in stacks)
+            {
+                stack.push(2);
+                Assert.Equal(1, stack.size());
+                Assert.Equal(2, stack.peek());
+            }
+        }
+
+        [Fact]
+        public void TestPopOperation()
+        {
+            foreach (var stack in stacks)
+            {
+                stack.push(2);
+                Assert.Equal(2, stack.pop());
+                Assert.True(stack.isEmpty());
+            }
+        }
+
+        [Fact]
+        public void TestMultipleOperationsExhaustively()
+        {
+            foreach (var stack in stacks)
+            {
+                Assert.True(stack.isEmpty());
+                stack.push(1);
+                Assert.False(stack.isEmpty());
+                stack.push(2);
+                Assert.Equal(2, stack.size());
+                Assert.Equal(2, stack.peek());
+                Assert.Equal(2, stack.size());
+                Assert.Equal(2, stack.pop());
+                Assert.Equal(1, stack.size());
+                Assert.Equal(1, stack.peek());
+                Assert.Equal(1, stack.size());
+                Assert.Equal(1, stack.pop());
+                Assert.Equal(0, stack.size());
+                Assert.True(stack.isEmpty());
+            }
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            stacks.Clear();
         }
     }
 }
