0c482f2590e122965318d6f7a0320d6fe4177e4f
Ham Rara <hmrara01@gmail.com>
2021-05-07 13:34:48 -0400

    Initial check-in for ArrayStack and IntStack

--- DataStructures.Core/Stack/ArrayStack.cs
+++ DataStructures.Core/Stack/ArrayStack.cs
@@ -1,49 +1,129 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DataStructures.Core
 {
     public class ArrayStack<T> : IStack<T>, IEnumerable<T>
     {
+        private int _size;
+        private int capacity;
+        private T[] data;
 
+        #region Constructors
+        public ArrayStack()
+        {
+            capacity = 16;
+            data = new T[capacity];
+        }
+        #endregion
 
         #region Public methods
+
+        public int size()
+        {
+            return _size;
+        }
+
         public bool isEmpty()
         {
-            throw new NotImplementedException();
-        }
-
-        public T peek()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T pop()
-        {
-            throw new NotImplementedException();
+            return (_size == 0);
         }
 
         public void push(T elem)
         {
-            throw new NotImplementedException();
+            if (_size == capacity)
+                increaseCapacity();
+
+            data[_size++] = elem;
         }
 
-        public int size()
+        public T peek()
         {
-            throw new NotImplementedException();
+            if (isEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            return data[_size - 1];
+        }
+
+        public T pop()
+        {
+            if(isEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            var elem = data[--_size];
+            data[_size] = default(T);
+
+            return elem;
+        }
+
+        #endregion
+
+        private void increaseCapacity()
+        {
+            capacity *= 2;
+            var newarr = new T[capacity];
+            Array.Copy(data, newarr, data.Length);
+            data = newarr;
+        }
+
+        #region Private enumerator class
+        private class ArrayStackEnumerator : IEnumerator<T>
+        {
+            T[] data;
+            int position = -1;
+            int _size;
+
+            // Constructor
+            public ArrayStackEnumerator(ArrayStack<T> stack)
+            {
+                data = stack.data;
+                _size = stack._size;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (position >= _size || position < 0) throw new InvalidOperationException("Index is out-of-range");
+                    return data[position];
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    if (position >= _size || position < 0) throw new InvalidOperationException("Index is out-of-range");
+                    return data[position];
+                }
+            }
+
+            public void Dispose(){}
+
+            public bool MoveNext()
+            {
+                position++;
+                return position < _size;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
         }
         #endregion
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new ArrayStackEnumerator(this);
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new ArrayStackEnumerator(this);
         }
     }
 }
---
+++ DataStructures.Core/Stack/IntStack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Core.Stack
+{
+    public class IntStack : IStack<int>, IEnumerable
+    {
+        private int[] arr;
+        private int pos = 0;
+
+        #region Constructors
+        public IntStack(int maxSize)
+        {
+            arr = new int[maxSize];
+        }
+        #endregion
+
+        #region Public methods
+        public int size()
+        {
+            return pos;
+        }
+
+        public bool isEmpty()
+        {
+            return (pos == 0);
+        }
+
+        public int peek()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            return arr[pos - 1];
+        }
+
+        public int pop()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            return arr[--pos];
+        }
+
+        public void push(int elem)
+        {
+            arr[pos++] = elem;
+        }
+        #endregion
+
+        #region Private enumerator class
+        private class IntStackEnumerator : IEnumerator
+        {
+            int[] arr;
+            int position = -1;
+            int pos;
+
+            // Constructor
+
+
+            public object Current => throw new NotImplementedException();
+
+            public bool MoveNext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        #endregion
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
